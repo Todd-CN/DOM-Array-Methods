563c5021015a83be585f9e033b8eb8b5cb3d7914
Todd Sutherland <todd.sutherland@cliniciannexus.com>
2021-07-13 13:11:34 -0500

    show millionaires using filter() method

--- script.js
+++ script.js
@@ -40,6 +40,14 @@ function sortByRichest() {
   updateDOM();
 }
 
+//Filter only millionaires
+function showMillionaires() {
+  data = data.filter((user) => {
+    return user.money > 1000000;
+  });
+  updateDOM();
+}
+
 // Add new object to data array
 function addData(obj) {
   data.push(obj);
@@ -76,3 +84,4 @@ function formatMoney(number) {
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
